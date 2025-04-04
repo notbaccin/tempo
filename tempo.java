8e69f5e3060f6f15d07632a7990a9310c8ce163a
Enzo Baccin <b4cc1n@gmail.com>
2025-04-03 21:36:06 -0300

    tempo.java

---
+++ tempo.java
@@ -0,0 +1,39 @@
+import javax.swing.JOptionPane;
+
+class tempo{
+    private int hora;
+    private int minuto;
+    private int segundo;
+
+    public tempo(int hora, int minuto, int segundo){
+        this.hora = hora;
+        this.minuto = minuto;
+        this.segundo = segundo;
+    }
+    public int getHora(){
+        return hora;
+    }
+    public void setHora(int hora){
+        this.hora = hora;
+    }
+    public int getMinuto(){
+        return minuto;
+    }
+    public void setMinuto(int minuto){
+        this.minuto = minuto;
+    }
+    public int getSegundo(){
+        return segundo;
+    }
+    public void setSegundo(int segundo){
+        this.segundo = segundo;
+    }
+    public int totalsegundos(){
+        if (int) {
+            totalsegundos() = hora*360
+            
+        }
+        return totalsegundos();
+    }
+
+    }
